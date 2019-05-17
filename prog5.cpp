e33f7aa46a27dd97a3a25bfb8bc183ed90a8e971
Tyler Filla <tylerfilla@hotmail.com>
2019-05-17 10:56:38 -0500

    Preparations

---
+++ prog5.cpp
@@ -0,0 +1,24 @@
+/*
+ * Tyler Filla
+ * OPC19 - Program 5
+ */
+
+// Bunch o' headers
+// These were prepared before the competition, so many may be unneeded
+#include <algorithm>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+int main(int argc, char* argv[]) {
+  std::cout << "Hello, program 5!\n";
+}
